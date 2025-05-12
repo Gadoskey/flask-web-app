f1c00f866acc02d647f20168476d8c8557528302
Yusuf Mustapha Opeyemi <138693302+Gadoskey@users.noreply.github.com>
2025-05-12 16:13:03 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,9 +1,7 @@
-from flaskblog import create_app
-
 def test_home_route():
     app = create_app()
     client = app.test_client()
 
     response = client.get('/')
+    
     assert response.status_code == 200
-    assert b"Hello from Flask App!" in response.data
